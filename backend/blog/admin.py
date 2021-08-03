04682c59191fa29b30a5a24421daba4ab67fbd37
JasonVolkoff <kashijason@gmail.com>
2021-08-02 20:31:53 -0700

    fixed admin display list

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import BlogPost
+from .models import BlogPost, Category
 
 # Apply summernote to all TextField in model.
 class BlogPostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
     exclude = ('slug',)
-    list_display = ('id', 'title', 'category', 'date_created')
+    list_display = ('id', 'title', 'date_created')
     list_display_links = ('id', 'title')
     search_fields = ('title',)
     list_per_page = 25
     summernote_fields = ('content',)
 
 admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(Category)
