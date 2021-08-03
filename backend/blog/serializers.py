4db398eb60a4618c24be5c859d88adaa6fba7938
JasonVolkoff <kashijason@gmail.com>
2021-08-02 18:26:05 -0700

    Added category serializer

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,8 +1,14 @@
-from rest_framework import serializers
-from .models import BlogPost
+from rest_framework import serializer
+from .models import BlogPost, Category
 
 class BlogPostSerializer(serializer.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = '__all__'
         lookup_field = 'slug'
+
+class CategorySerializer(serializer.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        lookup_field = 'name'
